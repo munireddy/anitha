9e0b623d70ba4327ae8331979c87797457997aee
root <root@ip-172-31-8-82.us-east-2.compute.internal>
2019-06-03 18:50:03 +0000

    count.rb

--- count.rb
+++ count.rb
@@ -4,14 +4,15 @@ f = File.open(the_file, "r")
 f.each_line { |line|
   words = line.split
   words.each { |w|
-    if h.has_key?(w)
-      h[w] = h[w] + 1
-    else
-      h[w] = 1
+    if w.length >= 3
+      if h.has_key?(w)
+        h[w] = h[w] + 1
+      else
+        h[w] = 1
+      end
     end
   }
 }
-
 puts "Word that appeared second hsighest number of tumes"
-puts h.invert.values[2]
+puts h.invert.values[-2]
 #puts h.invert.max&.last
